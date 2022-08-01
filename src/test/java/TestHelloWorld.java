024f3b4d4f0e4c38604cae8591c1d58b217552d6
Mitchell Johnson <mitchell.johnson624@gmail.com>
2022-08-01 17:41:16 -0400

    Hello World

--- src/test/java/TestHelloWorld.java
+++ src/test/java/TestHelloWorld.java
@@ -6,8 +6,13 @@ import static org.junit.Assert.assertEquals;
  * Created by LaunchCode
  */
 public class TestHelloWorld {
+    public void SayHello() {
+        assertEquals("Hello, World!", HelloWorld.sayHello());
+    }
 
-    @Test
+
+
+   @Test
     public void testSayHello() {
         assertEquals("Hello, World!", HelloWorld.sayHello());
     }
